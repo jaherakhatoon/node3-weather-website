9ee3875800471f16957817f9cfb80581c612a3a2
jahera <jahera.khatoon@codelogicx.com>
2021-09-07 17:13:58 +0530

    add forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
   const url =
-     "http://api.weatherstack.com/current?access_key=0aed906fa7c1f59246137e12ebcecbd7&query=" +
+    "http://api.weatherstack.com/current?access_key=0aed906fa7c1f59246137e12ebcecbd7&query=" +
     latitude +
     "," +
     longitude;
@@ -19,7 +19,9 @@ const forecast = (latitude, longitude, callback) => {
           body.current.temperature +
           " degrees out.It feels like " +
           body.current.feelslike +
-          " degrees out. There is a " +
+          " degrees out.Humidity is " +
+          body.current.humidity +
+          ".There is a " +
           body.current.precip +
           "% chance of rain"
       );
